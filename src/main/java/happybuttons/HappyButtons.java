6c2eeffad17777ab62bca10a42b9b2ae4c77ea90
Michael Balibrea <54125278+khelbalibrea@users.noreply.github.com>
2023-09-22 21:00:33 +0800

    Create repository

---
+++ src/main/java/happybuttons/HappyButtons.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package happybuttons;
+
+/**
+ *
+ * @author Michael Balibrea
+ */
+public class HappyButtons {
+
+    public static void main(String[] args) {
+        System.out.println("Test github!");
+    }
+}
