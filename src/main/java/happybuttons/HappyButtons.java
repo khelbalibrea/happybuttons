c5a65eed030759c56a1ad133c72b0d799e4da483
Michael Balibrea <54125278+khelbalibrea@users.noreply.github.com>
2023-09-26 23:54:03 +0800

    Multi push
    1. Create main frame java class
    2. Screen size fitting
    3. Checking of destination folders (BG, SFX, and main folder)

--- src/main/java/happybuttons/HappyButtons.java
+++ src/main/java/happybuttons/HappyButtons.java
@@ -4,13 +4,106 @@
 
 package happybuttons;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Michael Balibrea
  */
 public class HappyButtons {
-
+    public static String desktopPath = "";
+    public static MainFrame mf;
+    
     public static void main(String[] args) {
-        System.out.println("Test github!!");
+        mf = new MainFrame();
+        getDesktopPath();
+        checkMainFolder();
+        checkSubFolders();
+        mf.setVisible(true);
     }
+    
+    public static void getDesktopPath() {
+        String command = "powershell -command \"[Environment]::GetFolderPath('\"Desktop\"')\"";
+        
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+            
+            String line;
+            while ((line = reader.readLine()) != null) {
+                desktopPath = line;
+            }
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void checkMainFolder(){
+        if(desktopPath != "") {
+            try{
+                File mainPath = new File(desktopPath);
+
+                if(!mainPath.exists()){
+                    mainPath.mkdir();
+                }
+            } catch(Exception e){
+    //            Controller.chkMain = 1;
+            }
+        }
+    }
+    
+    public static void checkSubFolders(){
+        // checking BG folder
+        try{
+            File subPath1 = new File(desktopPath + "\\HappyButtons\\bg");
+           
+            if(!subPath1.exists()){
+                subPath1.mkdir();
+                JOptionPane.showMessageDialog(mf, 
+                    "\"" + desktopPath + "\\HappyButtons\\bg\" folder not found\n\n\"bg\" folder is created. Note that bg sounds involve in some profile saves may gone missing", 
+                    "CRITICAL FOLDER MISSING", 
+                    JOptionPane.WARNING_MESSAGE);
+            }
+        } catch(Exception e){
+//            Controller.chksub1 = 1;
+        }
+        
+        // checking SFX folder
+        try{
+            File subPath1 = new File(desktopPath + "\\HappyButtons\\sfx");
+           
+            if(!subPath1.exists()){
+                subPath1.mkdir();
+                JOptionPane.showMessageDialog(mf, 
+                    "\"" + desktopPath + "\\HappyButtons\\sfx\" folder not found\n\n\"sfx\" folder is created. Note that sfx sounds involve in some profile saves may gone missing", 
+                    "CRITICAL FOLDER MISSING", 
+                    JOptionPane.WARNING_MESSAGE);
+            }
+        } catch(Exception e){
+//            Controller.chksub1 = 1;
+        }
+    }
+    
+    // initialize XML as database
+//    public static void initializeDatabase(){
+//        File dbPath = new File(desktopPath + "\\HappyButtons\\happyDB.xml");
+//
+//        for(ctr = 0; ctr < profileDB.length; ctr++) {
+//            if(dbPath.exists()){
+//                ProfileDatabase[] profile = new BeanHelper().readFromXml();
+//                profileDB = profile;
+//            } else {
+//                noDB = 1;
+//                profileDB[ctr] = new ProfileDatabase();
+//            }
+//        }
+//    }
 }
